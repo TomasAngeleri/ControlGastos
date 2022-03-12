03875a062a2d130fe6868fe7c306fa90cddd5fee
TomasAngeleri <44233271+TomasAngeleri@users.noreply.github.com>
2022-03-12 02:00:12 -0300

    Componente Presupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-
+import Header from './components/Header';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Control de gastos</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
