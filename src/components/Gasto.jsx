c5e93dd563167c131e4cea7c3c9a8e06e5f2d33a
TomasAngeleri <44233271+TomasAngeleri@users.noreply.github.com>
2022-07-11 23:12:26 -0300

    Add format fecha

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { formatFecha } from '../helpers';
+
 
 const Gasto = ({ gasto }) => {
   const { categoria, nombre, monto, id, fecha } = gasto;
   return (
     <div className='gasto sombra'>
       <div className='contenido-gasto'>
+        {/* IMAGEN */}
         <div className='descripcion-gasto'>
           <p className='categoria'>{categoria}</p>
           <p className='nombre-gasto'>{nombre}</p>
-          <p className='cantidad-gasto'>{monto}</p>
+          <p className='fecha-gasto'>
+            Agregado el:{''}
+            <span>{formatFecha(fecha)}</span>
+          </p>
         </div>
       </div>
+      <p className='cantidad-gasto'>$ {monto}</p>
     </div>
   )
 }
